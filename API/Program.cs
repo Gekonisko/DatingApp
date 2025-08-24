58772373c15a5259c07b02e33fca69e61b50ce6e
Gekonisko <jakubocytko@student.agh.edu.pl>
2025-08-24 12:04:00 +0200

    add angular project and set ssl cert

--- API/Program.cs
+++ API/Program.cs
@@ -16,6 +16,8 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200", "http://localhost:4200"));
+
 app.MapControllers();
 
 app.Run();
